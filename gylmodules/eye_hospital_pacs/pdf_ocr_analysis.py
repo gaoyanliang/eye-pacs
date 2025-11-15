587dd1c9d074e3d4c8fde4c80964160dfbf1ae3a
gaoyanliang <yanliang2025@163.com>
2025-11-15 10:36:42 +0800

    update

--- gylmodules/eye_hospital_pacs/pdf_ocr_analysis.py
+++ gylmodules/eye_hospital_pacs/pdf_ocr_analysis.py
@@ -4,14 +4,15 @@ import json
 import re
 
 from pathlib import Path
-import numpy as np
 from datetime import datetime
+
+import numpy as np
 from PIL import Image
 from paddleocr import PaddleOCR
 import time
 import io
 import os
-from typing import Union, List, Dict, Literal
+from typing import Literal
 from pdf2image import convert_from_path
 import cv2
 
@@ -139,6 +140,20 @@ class OCRProcessor:
             # 如果你只关心文本，可直接拼接
             full_text = " ".join([t for t in texts if t.strip()])
             # print(full_text)
+            return full_text
+        except Exception as e:
+            print(datetime.now(), f"OCR处理失败: {str(e)}")
+            return ''
+
+    def captcha(self, img):
+        try:
+            # OCR 识别
+            results = self.ocr_engine.predict(img)
+            page = results[0]
+            # 直接获取 rec_texts
+            texts = page["rec_texts"]
+            # 如果你只关心文本，可直接拼接
+            full_text = " ".join([t for t in texts if t.strip()])
             return full_text
         except Exception as e:
             print(datetime.now(), f"OCR处理失败: {str(e)}")
@@ -623,23 +638,24 @@ def regularly_parsing_eye_report():
 """解析翻转拍网站验证码"""
 
 
-def ocr_captcha(img_b64):
-    if not img_b64:
+def ocr_captcha(base64_str):
+    if not base64_str:
         return ''
-    import base64
+
     try:
-        img_bytes = base64.b64decode(img_b64)
-        # 保存图像文件以便调试
-        timestamp = datetime.now().strftime("%Y%m%d%H%M%S")
-        image_filename = f"captcha_image_{timestamp}.png"
-        """保存图像文件"""
-        with open(image_filename, 'wb') as f:
-            f.write(img_bytes)
+        import base64
+        if "," in base64_str:
+            base64_str = base64_str.split(",")[1]
+
+        img_data = base64.b64decode(base64_str)
+        img_array = np.frombuffer(img_data, np.uint8)
+        img = cv2.imdecode(img_array, cv2.IMREAD_COLOR)
 
         processor = OCRProcessor()
-        ret_data = processor.ocr_engine.predict(image_filename)
+        ret_data = processor.captcha(img)
         return ret_data
     except Exception as e:
+        print(e)
         return ''
 
 
@@ -664,7 +680,7 @@ if __name__ == "__main__":
     # file_path = r"E:\pdf_share\生物力学-横版.pdf"
     # file_path = r"E:\pdf_share\生物力学-竖版.pdf"
     # file_path = r"E:\pdf_share\眼底照片.pdf"
-    # file_path = r"E:\pdf_share\Master700.pdf"
+    file_path = r"E:\pdf_share\Master700.pdf"
     # file_path = r"E:\pdf_share\阿玛仕手术报告.pdf"
     # file_path = "/Users/gaoyanliang/各个系统文档整理/眼科医院/眼科医院仪器检查报告和病历/已经解析的所有病历/屈光四图-横版.pdf"
     # file_path = "/Users/gaoyanliang/各个系统文档整理/眼科医院/眼科医院仪器检查报告和病历/已经解析的所有病历/Master700.pdf"
@@ -674,14 +690,10 @@ if __name__ == "__main__":
     # print(final_file_name)
     # print(machine)
     # print(values)
-    # #
-    # # for k,v in values.items():
-    # #     values[k] = str(v).replace('mm', '').replace('μm', '').replace('mm', '')
-    #
-    # for k,v in values.items():
-    #     print(k, v)
 
 
+    # data = '/9j/4AAQSkZJRgABAgAAAQABAAD/2wBDAAgGBgcGBQgHBwcJCQgKDBQNDAsLDBkSEw8UHRofHh0aHBwgJC4nICIsIxwcKDcpLDAxNDQ0Hyc5PTgyPC4zNDL/2wBDAQkJCQwLDBgNDRgyIRwhMjIyMjIyMjIyMjIyMjIyMjIyMjIyMjIyMjIyMjIyMjIyMjIyMjIyMjIyMjIyMjIyMjL/wAARCAA8AKADASIAAhEBAxEB/8QAHwAAAQUBAQEBAQEAAAAAAAAAAAECAwQFBgcICQoL/8QAtRAAAgEDAwIEAwUFBAQAAAF9AQIDAAQRBRIhMUEGE1FhByJxFDKBkaEII0KxwRVS0fAkM2JyggkKFhcYGRolJicoKSo0NTY3ODk6Q0RFRkdISUpTVFVWV1hZWmNkZWZnaGlqc3R1dnd4eXqDhIWGh4iJipKTlJWWl5iZmqKjpKWmp6ipqrKztLW2t7i5usLDxMXGx8jJytLT1NXW19jZ2uHi4+Tl5ufo6erx8vP09fb3+Pn6/8QAHwEAAwEBAQEBAQEBAQAAAAAAAAECAwQFBgcICQoL/8QAtREAAgECBAQDBAcFBAQAAQJ3AAECAxEEBSExBhJBUQdhcRMiMoEIFEKRobHBCSMzUvAVYnLRChYkNOEl8RcYGRomJygpKjU2Nzg5OkNERUZHSElKU1RVVldYWVpjZGVmZ2hpanN0dXZ3eHl6goOEhYaHiImKkpOUlZaXmJmaoqOkpaanqKmqsrO0tba3uLm6wsPExcbHyMnK0tPU1dbX2Nna4uPk5ebn6Onq8vP09fb3+Pn6/9oADAMBAAIRAxEAPwDtrW1ga1hZoIySikkoOeKsCztv+feL/vgU2z/484P+ua/yqyKiMY8q0IjGPKtCIWdr/wA+0P8A3wKeLK1/59of+/YqUU4U+WPYfLHsRCytP+fWH/v2KcLG0/59YP8Av2Kg1DVtP0mISX95DbqenmMAT9B3p+m6tp+rQmWwuo50HUoen4VfsHy8/Lp3toHLHaxOLCz/AOfWD/v2KcLCz/59IP8Av2KnFOFRyx7Byx7EI0+y/wCfS3/79j/CnDTrL/nzt/8Av0v+FT5AGScAd6oSa7p8F4LaacRHGd8nyoDuK4JPQ5HGevGM1SpqWyDlj2LY06x/587f/v0v+FOGm2P/AD5W/wD36X/Cp0IZQQcgjIp4qeWPYOWPYrjTLD/nytv+/S/4U8aZYf8APjbf9+l/wqwKeKOWPYOWPYrDS9P/AOfG2/78r/hTxpWn/wDPha/9+V/wqyKeKOWPYOWPYqjStO/58LX/AL8r/hThpOnf9A+1/wC/K/4VJHd20spjjuInkBwVVwSD9Ksim4JboOWPYqjSdN/6B9p/35X/AAqtqel6fHpF66WNqrrA5VhCoIO08jitYVV1b/kC3/8A17yf+gmplGPK9BSjHlehyVn/AMecH/XNf5VZFV7P/jzg/wCua/yqyKcfhQ4/ChwqjrerQ6Ho1zqMwysKZC/3j2FXxXI/E20nu/BU4gUsY5FkYD+6M5rrwVKFbE06c9m0n94Sdk2jz7QtD1H4lavdX+o3rxwRnBIGdpPRVHQCmaWbrwH48itvOaS2ebymx/GucdPWovA3jqLwlbXcE9o86TMHGwgEHGO9Yuva9d6vr41eSLyTvDxLjgYORX6L9WxdTEVcPOKWH5bRWlttLdbnJeKSfU9r8ReNLbSLt7CdZIfMi/dzjB+YjI/wz61y/hj4iyW+6HUJWuiXCRqMZGeEXPf3NavifTb/AFLRNHS2slvbedY/MP8AFHnkn6Yb8wKydD8Faz4Z1qFVtoLuOcEF2UMsfOBk/QmvlsNRy/6m/afxH0vva/3en3G7c+bTY9Mu5p7nRGltUYyPHkKvBPHQE9PrXi9v4W1TxJqc9veuY5dxlWMbVL464LfNnnrznv617zGpVApxx6DArmvGE93pkEd/p8RMsec7SVBBHzbyAeMdOhyK8/LcdUw0nGilzS2b6Fzinueb+DPEF74d1ybRrpmbyHYKjbCqDv8AdBPv8rY9j1HuccqPCsoZdjAEHPFfPHhTTr3xR41fUDKvmLL5jsH5znrwOfXI+vrXo/jLTfGGooNM0N2S2DBXbCRqy43Zyecg8cV6Wd4WlPGwhzKEmk5Pon1IpyfLc6i+8aeHNMkMd1q9srjqofcf0qSw8Z+G9SkEdrrNoznorPtJ+gOM15fbfBmOCIXHiDxFDAT1EYGP++3I/lUsnwZ02+jZtD8TxzSKPusFkH4lTx+RrF4LKUuX6w79+V2/zHzVOx3vjbxzbeELEHy/OupBmJM8H615zp1v45+KAed9R+waVuKnaWVG9go+9+NesXHhPSdV0+1g1fT7e5khjVSxB6gAcHg9qkvrzR/BPh152Rbaytx8sadWJ6Ae5rDB42lQoqGHpc1Zvdq/3LuOUW3dvQ+e/FPh/U/h14htRFqLPIyCWKZCVOAe4+tfR/hjU31rwzp2oyLiS4gV2Hv3rwrT9O1b4veMZNQulaDTYSFY9o07Ip7k19CWVnDYWUNpboEhhQIijsBXocRVk6VGlWadaK95rpfp/X6kUVq2tiyKq6t/yBL/AP69pP8A0E1bFVdX/wCQJf8A/XtJ/wCgmvk5fCzWXws5Kz/48oP+ua/yqyKr2X/HlB/1zX+VWRRH4UEfhQ4USLG0TiUKY8HcG6YpRTiodSrDIIwRVFHm6r8NpfEC2628Lzs2S+SIlb35A/pXGfEa8sb7xHHDphjaKFBGnlY2kYHT3zkflXVa18H/ALXqM1xpt+kMUrFvKlUnbnsCO1XtA+Elpp10txqF39qKg4RVwufWvtcNjsuwso4n28pyUbKLu9fyOZxnL3bWO90WMxaNZxEcRwqin1UDAP5YrRFMVQoAAAA7CpBXxcnzNs6ShrN5c2OnPNawNNIDjCjJUeuO+PSuA8IeNZdb1aOz1cxmQPsKvtVSRjBAYjncvQZIyRjGNvpssMc8TRyqGRuoNed+KPh7PM89xpE0iXE3y5EpQnOD8xCndyP4iMcc9q9PL5YWUZUq6s3tLsRPm3RyPjiK28OeNYr/AEqfyoZmAkjtXQbVwAwUA5B+91GM11niTXfF2oeH4J9FjmhkxGkn2VA+9irGTnBKhSFwQRnca4eTwxr13fhNXupJ44mQZeWQfMDj5ty5HGeTj2JAxXuXhqwksNJjSY/vT1UKVVQOAADzjvk8kknvXr5liKNCFBrlqSgrNvVNdP8ALX8zOCbb6HkNj8JfE+vEXutah5Mj4z57tJL2/wDr9/Sn3PwX8QWBW40zUYZJkwRtYxtnjofrnv2r3UU8Vxf6yY++jSj2srFexiZ/h5b1fD2nrqIcXwgQXG9tx8zHzc9+c15l8YdK1/Xda0zT9MsLm6t1hMh8pDsDlsfMeg4Hf1r2AV5t8XNL8Q6pBpsOhreyKzOk0Vs5CvnGN+DjAx1PrWGT1+THxqLljvq9ldP+tx1F7ljr/BWkLofhDTbHyUimSFTOqsG/ekfPyODzn8q6EVy/w+0rUdE8E6fpuqxrHdQBwVVw2AXJHI46GupFcGLd8RN83Nq9e+u/zLjshwqrq/8AyBL/AP69pP8A0E1bFVdX/wCQJf8A/XtJ/wCgmuWXwsUvhZyVl/x5W/8A1zX+VWRXMxa1cxRJGqREIoUZB7fjUn9v3X/POH/vk/41lGtGyM41Y2R0opwrmf8AhIbv/nnB/wB8n/Gl/wCEiu/+ecH/AHyf8ar20R+2idQKeK5X/hJLz/nlB/3yf8aX/hJbz/nlB/3yf8aPbRD20TrBTxXI/wDCT3v/ADyt/wDvlv8AGl/4Si9/55W//fLf40e2iHtonXiniuO/4Sq+/wCeVv8A98t/jS/8JXff88rb/vlv8aPbRD20TswKeK4r/hLb/wD5423/AHy3+NL/AMJfqH/PG2/75b/4qj20Q9tE7YU8Vw//AAmGof8APG1/75b/AOKpf+Ey1H/nja/98t/8VR7aIe2id0KeK4P/AITPUf8Anja/98N/8VS/8JrqX/PC0/74b/4qj20Q9tE70U8VwH/Cbal/zwtP++G/+Kpf+E41P/nhaf8AfDf/ABVHtoh7aJ6CKq6v/wAgPUP+vaT/ANBNcV/wnOp/88LT/vhv/iqjufGeo3VrNbvDahJUZGKq2QCMcfNUyrRsxSqxsz//2Q=='
+    # print(ocr_captcha(data))
 
 
 
